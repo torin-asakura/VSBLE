3b9f022bebb0b3a90f89571e099f4a42958fd115
Nikita Elfimov <ne@atls.team>
2022-03-30 16:42:45 +0500

    chore(call-to-actions): display without user

--- site/fragments/call-to-actions/src/sign-up.component.tsx
+++ site/fragments/call-to-actions/src/sign-up.component.tsx
@@ -1,3 +1,5 @@
+import { WithoutUser }      from '@atls/react-user'
+
 import React                from 'react'
 import { FC }               from 'react'
 import { FormattedMessage } from 'react-intl'
@@ -13,47 +15,49 @@ const doNothing = () => {
 }
 
 const SignUpCTA: FC = () => (
-  <Row>
-    <Column alignItems='center' fill>
-      <Layout flexBasis={16} />
-      <Layout width={['100%', '100%', 1440]} justifyContent='center'>
-        <Layout flexBasis={24} />
-        <Layout flexDirection={['column', 'column', 'row']}>
-          <Tile
-            actionName={
-              <FormattedMessage
-                id='call_to_actions.sign_up_as_a_brand'
-                defaultMessage='Sign Up as a Brand'
-              />
-            }
-            content={
-              <FormattedMessage
-                id='call_to_actions.find_selected_creators_worldwide'
-                defaultMessage='Find selected creators worldwide'
-              />
-            }
-            action={doNothing}
-          />
-          <Tile
-            actionName={
-              <FormattedMessage
-                id='call_to_actions.sign_up_as_a_creator'
-                defaultMessage='Sign Up as a Creator'
-              />
-            }
-            content={
-              <FormattedMessage
-                id='call_to_actions.showcase_your_work_and_win_clients'
-                defaultMessage='Showcase your work and wind clients'
-              />
-            }
-            action={doNothing}
-          />
+  <WithoutUser>
+    <Row>
+      <Column alignItems='center' fill>
+        <Layout flexBasis={16} />
+        <Layout width={['100%', '100%', 1440]} justifyContent='center'>
+          <Layout flexBasis={24} />
+          <Layout flexDirection={['column', 'column', 'row']}>
+            <Tile
+              actionName={
+                <FormattedMessage
+                  id='call_to_actions.sign_up_as_a_brand'
+                  defaultMessage='Sign Up as a Brand'
+                />
+              }
+              content={
+                <FormattedMessage
+                  id='call_to_actions.find_selected_creators_worldwide'
+                  defaultMessage='Find selected creators worldwide'
+                />
+              }
+              action={doNothing}
+            />
+            <Tile
+              actionName={
+                <FormattedMessage
+                  id='call_to_actions.sign_up_as_a_creator'
+                  defaultMessage='Sign Up as a Creator'
+                />
+              }
+              content={
+                <FormattedMessage
+                  id='call_to_actions.showcase_your_work_and_win_clients'
+                  defaultMessage='Showcase your work and wind clients'
+                />
+              }
+              action={doNothing}
+            />
+          </Layout>
+          <Layout flexBasis={24} />
         </Layout>
-        <Layout flexBasis={24} />
-      </Layout>
-    </Column>
-  </Row>
+      </Column>
+    </Row>
+  </WithoutUser>
 )
 
 export { SignUpCTA }
